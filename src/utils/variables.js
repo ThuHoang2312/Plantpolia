6b6004fc9199f71e43dfd4ac9373c10f301ff0eb
Chi Nguyen <chi.nguyen@live.com>
2023-02-21 14:05:22 +0200

    [addPlant]install packages (image picker, dropdown picker) & add colors, tags to utils

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,4 +1,6 @@
 const baseUrl = 'https://media.mw.metropolia.fi/wbma/';
 const uploadUrl = 'https://media.mw.metropolia.fi/wbma/uploads/';
+const appTag = 'plantpoliaPrimaryPlant';
+const userTag = 'plantpoliaUserPlant';
 
 export {baseUrl, uploadUrl};
