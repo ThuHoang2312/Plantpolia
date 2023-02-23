3b4039312b6b17b4af2602a13eebb9c19d6d1779
ericaskari <ericaskari@gmail.com>
2023-02-23 22:47:17 +0200

    [feature/context-refactoring] Context Type Added

--- src/contexts/MainContext.js
+++ src/contexts/MainContext.js
@@ -2,59 +2,74 @@ import React, {createContext, useContext, useState} from 'react';
 import PropTypes from 'prop-types';
 
 /** @type {import('../types/MainContextModel').MainContextReactContext} */
-export const MainContext = createContext({
-  isLoggedIn: false,
-  setIsLoggedIn: () => {},
-});
+export const MainContext = createContext(null);
 
 export const MainProvider = (props) => {
+  /** @type {import('../types/MainContextModel').IsLoggedInUseStateModel} */
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [user, setUser] = useState({});
+
+  /** @type {import('../types/MainContextModel').UserUseStateModel} */
+  const [user, setUser] = useState(null);
+
+  /** @type {import('../types/MainContextModel').UpdateUseStateModel} */
   const [update, setUpdate] = useState(true);
+
+  /** @type {import('../types/MainContextModel').LastWaterUseStateModel} */
   const [lastWater, setLastWater] = useState('');
+
+  /** @type {import('../types/MainContextModel').NotificationTimeUseStateModel} */
   const [notificationTime, setNotificationTime] = useState('');
+
+  /** @type {import('../types/MainContextModel').ImageUseStateModel} */
   const [image, setImage] = useState('');
+
+  /** @type {import('../types/MainContextModel').ImageSelectedUseStateModel} */
   const [imageSelected, setImageSelected] = useState(false);
+
+  /** @type {import('../types/MainContextModel').TypeUseStateModel} */
   const [type, setType] = useState('image');
+
+  /** @type {import('../types/MainContextModel').UploadUseStateModel} */
   const [upload, setUpload] = useState(false);
+
+  /** @type {import('../types/MainContextModel').TokenUseStateModel} */
   const [token, setToken] = React.useState('token');
 
-  return (
-    <MainContext.Provider
-      value={{
-        isLoggedIn,
-        setIsLoggedIn,
-        user,
-        setUser,
-        update,
-        setUpdate,
-        lastWater,
-        setLastWater,
-        notificationTime,
-        setNotificationTime,
-        image,
-        setImage,
-        upload,
-        setUpload,
-        type,
-        setType,
-        imageSelected,
-        setImageSelected,
-        token,
-        setToken,
-      }}
-    >
-      {props.children}
-    </MainContext.Provider>
-  );
-};
+  /** @type {import('../types/MainContextModel').MainContextModel} */
+  const state = {
+    isLoggedIn,
+    setIsLoggedIn,
+    user,
+    setUser,
+    update,
+    setUpdate,
+    lastWater,
+    setLastWater,
+    notificationTime,
+    setNotificationTime,
+    image,
+    setImage,
+    upload,
+    setUpload,
+    type,
+    setType,
+    imageSelected,
+    setImageSelected,
+    token,
+    setToken,
+  };
 
-MainProvider.propTypes = {
-  children: PropTypes.node,
+  return (
+    <MainContext.Provider value={state}>{props.children}</MainContext.Provider>
+  );
 };
 
 export const useMainContext = () => {
   const context = useContext(MainContext);
 
   return context;
+};
+
+MainProvider.propTypes = {
+  children: PropTypes.node,
 };
--- src/types/MainContextModel.d.ts
+++ src/types/MainContextModel.d.ts
@@ -1,16 +1,97 @@
-import {Context} from 'react';
+import {Context, Dispatch} from 'react';
+import {IUserModel} from './UserModel';
 
-export interface MainContextModel extends Record<string, any> {
+export interface MainContextModel {
+  /**
+   * When it's true user is logged in.
+   */
   isLoggedIn: boolean;
-  setIsLoggedIn: (isLoggedIn: boolean) => void;
-  // userProfile: IUserModel | null;
-  // accessToken: (accessToken: string | null) => void;
-  // expirationDate: string | number;
-  // setUserProfile: (userModel: IUserModel) => void;
-  // setAccessToken: (accessToken: string) => void;
-  // setExpirationDate: (date: string) => void;
-  // needsUpdate: boolean;
-  // setNeedsUpdate: (needsUpdateNewValue: boolean) => void;
+  /**
+   * TODO: Write description.
+   */
+  setIsLoggedIn: Dispatch<boolean>;
+  /**
+   * Logged in user's UserProfile.
+   */
+  user: IUserModel | null;
+  /**
+   * TODO: Write description.
+   */
+  setUser: Dispatch<IUserModel | null>;
+  /**
+   * When true means data needs to be updated.
+   */
+  update: boolean;
+  /**
+   * TODO: Write description.
+   */
+  setUpdate: Dispatch<boolean>;
+  /**
+   * TODO: Write description.
+   */
+  lastWater: any;
+  /**
+   * TODO: Write description.
+   */
+  setLastWater: Dispatch<any>;
+  /**
+   * TODO: Write description.
+   */
+  notificationTime: any;
+  setNotificationTime: Dispatch<any>;
+  /**
+   * TODO: Write description.
+   */
+  image: any;
+  /**
+   * TODO: Write description.
+   */
+  setImage: Dispatch<any>;
+  /**
+   * TODO: Write description.
+   */
+  upload: any;
+  setUpload: Dispatch<any>;
+  /**
+   * TODO: Write description.
+   */
+  type: any;
+  /**
+   * TODO: Write description.
+   */
+  setType: Dispatch<any>;
+  /**
+   * TODO: Write description.
+   */
+  imageSelected: boolean;
+  /**
+   * TODO: Write description.
+   */
+  setImageSelected: Dispatch<boolean>;
+  /**
+   * Access token of the user.
+   */
+  token: string | null;
+  /**
+   * TODO: Write description.
+   */
+  setToken: Dispatch<string | null>;
 }
+
+type UseStateModel<VarName extends keyof MainContextModel> = [
+  MainContextModel[VarName],
+  Dispatch<MainContextModel[VarName]>
+];
+
+export type IsLoggedInUseStateModel = UseStateModel<'isLoggedIn'>;
+export type UserUseStateModel = UseStateModel<'user'>;
+export type UpdateUseStateModel = UseStateModel<'update'>;
+export type LastWaterUseStateModel = UseStateModel<'lastWater'>;
+export type NotificationTimeUseStateModel = UseStateModel<'notificationTime'>;
+export type ImageUseStateModel = UseStateModel<'image'>;
+export type ImageSelectedUseStateModel = UseStateModel<'imageSelected'>;
+export type TypeUseStateModel = UseStateModel<'type'>;
+export type UploadUseStateModel = UseStateModel<'upload'>;
+export type TokenUseStateModel = UseStateModel<'token'>;
 
 export type MainContextReactContext = Context<MainContextModel>;
