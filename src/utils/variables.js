ad7ffdb8cfb581e223953ff03e58674104970ba8
Chi Nguyen <chi.nguyen@live.com>
2023-02-24 10:24:41 +0200

    [addPlant-Search] add display for not found plant, suggest form if user wish to request for adding plant to lib

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -3,5 +3,6 @@ const uploadUrl = 'https://media.mw.metropolia.fi/wbma/uploads/';
 const appTag = 'plantpoliaPrimaryPlant';
 const userTag = 'plantpoliaUserPlant';
 const userAccountTag = 'plantpolia.';
+const requestTag = 'plantpoliaRequest';
 
-export {baseUrl, uploadUrl, userTag, userAccountTag, appTag};
+export {baseUrl, uploadUrl, userTag, userAccountTag, appTag, requestTag};
