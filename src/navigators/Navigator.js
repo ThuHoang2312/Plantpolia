470b33a5e22cb8cac91692877da27ab0735b16e1
Mohammad Askari (Eric) <32340808+ericaskari@users.noreply.github.com>
2023-02-24 12:55:15 +0200

    Feature/splash screen (#10)
    * [feature/context-refactoring] Reformated the file with prettier
    
    * [feature/context-refactoring] few types and context hook added.
    
    * [feature/context-refactoring] Context Type Added
    
    * [feature/splash-screen] Splash screen added

--- src/navigators/Navigator.js
+++ src/navigators/Navigator.js
@@ -122,6 +122,19 @@ const StackScreen = () => {
             options={{headerShown: false}}
           />
         </>
+      ) : isExpired ? (
+        <>
+          <Stack.Screen
+            name="ExpiredToken"
+            component={ExpiredToken}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{headerShown: false}}
+          />
+        </>
       ) : (
         <Stack.Screen
           name="Login"
