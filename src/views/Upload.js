2d47fa7e5b1cc6e2d2fd0050ec17d3b5476fc882
ericaskari <ericaskari@gmail.com>
2023-02-28 04:48:45 +0200

    [state-refactoring] Prop Cleanup

--- src/views/Upload.js
+++ src/views/Upload.js
@@ -65,7 +65,7 @@ const Upload = ({navigation, route}) => {
     // Clear picker choices
     setLastWater('');
     setNotificationTime('');
-    setPlantLocation('');
+    // setPlantLocation('');
   };
 
   const errorHandler = () => {
