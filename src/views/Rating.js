22b5963ac62430b6724b53404bc33bcf4f1a6934
Chi Nguyen <chi.nguyen@live.com>
2023-03-09 20:06:47 +0200

    remove unused export

--- src/views/Rating.js
+++ src/views/Rating.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext} from 'react';
 import PropTypes from 'prop-types';
 import {StyleSheet, View, Text, Alert} from 'react-native';
 import {AirbnbRating} from '@rneui/themed';
