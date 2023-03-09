96c388fae406655e0e5d2d905496fc701a4f910a
Chi Nguyen <chi.nguyen@live.com>
2023-03-09 10:26:50 +0200

    [feature/donut] add donut pie in home page

--- src/components/Home/Welcome.js
+++ src/components/Home/Welcome.js
@@ -1,29 +1,87 @@
 import React, {useContext} from 'react';
+import PropTypes from 'prop-types';
 import {StyleSheet, Text, View} from 'react-native';
+import {VictoryPie} from 'victory-native';
 import {MainContext} from '../../contexts/MainContext';
 import {colors} from '../../utils/colors';
 import {fontFamily, fontSizes, spacing} from '../../utils/sizes';
+import {StatisticCard} from '../StatisticCard';
 
-const Welcome = () => {
+const Welcome = ({dehydratedUserPlants, hydratedUserPlants}) => {
   const {user} = useContext(MainContext);
+  const data = [
+    {
+      x: 'Hydrated Plants',
+      y: hydratedUserPlants.length,
+    },
+    {
+      x: 'Dehydrated Plants',
+      y: dehydratedUserPlants.length,
+    },
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome back {user.full_name}!</Text>
-    </View>
+    <>
+      <View style={styles.container}>
+        <View style={styles.pieContainer}>
+          <VictoryPie
+            data={data}
+            width={150}
+            height={150}
+            innerRadius={50}
+            colorScale={[colors.dehydrated, colors.hydrated]}
+            labelRadius={({innerRadius}) => innerRadius + 5}
+            style={{
+              labels: {
+                fill: colors.background,
+                fontSize: 20,
+                fontWeight: 'bold',
+              },
+            }}
+          />
+        </View>
+        <View style={styles.welcome}>
+          <Text style={styles.title}>Welcome {user.full_name}!</Text>
+        </View>
+      </View>
+      <View style={styles.statisticContainer}>
+        <StatisticCard number={hydratedUserPlants.length} isHydrated={true} />
+        <StatisticCard
+          number={dehydratedUserPlants.length}
+          isHydrated={false}
+        />
+      </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    marginHorizontal: spacing.md,
-    marginVertical: spacing.md,
     flexDirection: 'row',
-    backgroundColor: colors.background,
+    justifyContent: 'center',
+  },
+  pieContainer: {
+    justifyContent: 'center',
+    marginHorizontal: spacing.md,
+  },
+  welcome: {
+    justifyContent: 'center',
+    marginHorizontal: spacing.md,
   },
   title: {
     fontFamily: fontFamily.regular,
     color: colors.primary800,
     fontSize: fontSizes.lg,
+    textAlign: 'center',
+  },
+  statisticContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
 });
+
+Welcome.propTypes = {
+  hydratedUserPlants: PropTypes.array,
+  dehydratedUserPlants: PropTypes.array,
+};
 export default Welcome;
---
+++ src/components/StatisticCard.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {StyleSheet, View, Text} from 'react-native';
+import {spacing, fontFamily, fontSizes} from '../utils/sizes';
+import {colors} from '../utils/colors';
+
+export const StatisticCard = ({number, isHydrated}) => {
+  return (
+    <>
+      {isHydrated ? (
+        <View style={styles.container}>
+          <View style={styles.dehydrateWrapper}>
+            <Text style={styles.dehydrate}>{number}</Text>
+          </View>
+          <View style={styles.textWrapper}>
+            <Text style={styles.text}>NEED WATER</Text>
+          </View>
+        </View>
+      ) : (
+        <View style={styles.container}>
+          <View style={styles.hydrateWrapper}>
+            <Text style={styles.hydrate}>{number}</Text>
+          </View>
+          <View style={styles.textWrapper}>
+            <Text style={styles.text}>IS HEALTHY</Text>
+          </View>
+        </View>
+      )}
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: spacing.md,
+    flexDirection: 'row',
+    backgroundColor: colors.background,
+    justifyContent: 'center',
+    alignContent: 'center',
+    borderColor: colors.primary300,
+    borderWidth: spacing.sm / 4,
+    padding: spacing.sm,
+    borderRadius: spacing.sm,
+  },
+  dehydrate: {
+    color: colors.primary50,
+    fontSize: fontSizes.lg,
+    textAlign: 'center',
+    paddingHorizontal: spacing.sm,
+  },
+  hydrate: {
+    color: colors.primary50,
+    fontSize: fontSizes.lg,
+    textAlign: 'center',
+    paddingHorizontal: spacing.sm,
+  },
+  text: {
+    fontFamily: fontFamily.regular,
+    fontSize: fontSizes.md,
+    textAlign: 'center',
+  },
+  hydrateWrapper: {
+    backgroundColor: colors.hydrated,
+    borderRadius: spacing.sm / 2,
+  },
+  dehydrateWrapper: {
+    backgroundColor: colors.dehydrated,
+    borderRadius: spacing.sm / 2,
+  },
+  textWrapper: {
+    justifyContent: 'center',
+    marginHorizontal: spacing.sm / 2,
+  },
+});
+
+StatisticCard.propTypes = {
+  number: PropTypes.number,
+  isHydrated: PropTypes.bool,
+};
