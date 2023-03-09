27926d0fbd9e844aee2532332ac2854acd06a0d2
Mohammad Askari (Eric) <32340808+ericaskari@users.noreply.github.com>
2023-03-09 20:39:27 +0200

    [Features/Features/theme-provider] Theme Added (#60)

---
+++ src/utils/theme.js
@@ -0,0 +1,33 @@
+import {createTheme} from '@rneui/themed';
+import {colors} from './colors';
+import {fontFamily} from './sizes';
+
+export const theme = createTheme({
+  lightColors: {
+    primary: colors.primary500,
+  },
+  mode: 'light',
+  components: {
+    Button: {
+      titleStyle: {
+        fontFamily: fontFamily.regular,
+      },
+      buttonStyle: {},
+      style: {},
+      containerStyle: {},
+    },
+    Text: {
+      style: {
+        fontFamily: fontFamily.regular,
+      },
+      h1Style: {
+        fontFamily: fontFamily.regular,
+      },
+    },
+    Input: {
+      inputStyle: {
+        fontFamily: fontFamily.regular,
+      },
+    },
+  },
+});
