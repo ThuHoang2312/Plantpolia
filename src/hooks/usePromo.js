4a1a26362134005c8c689a7501485da9fcd03e64
ericaskari <ericaskari@gmail.com>
2023-03-09 22:44:14 +0200

    [Features/promo-logic] Context logic for promo

--- src/hooks/usePromo.js
+++ src/hooks/usePromo.js
@@ -1,9 +1,6 @@
 import {useState} from 'react';
-import {useLogger} from '../services/useLogger';
 
 export const usePromoHooks = ({defaultPromoStatus}) => {
-  const {log} = useLogger('usePromoHooks');
-
   const [promoStatus, setPromoStatus] = useState(defaultPromoStatus); //  'VIEWED' or 'NOT VIEWED'
 
   return {
--- src/views/Intro.js
+++ src/views/Intro.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {View} from 'react-native';
 import PropTypes from 'prop-types';
 import {useMainContext} from '../contexts/MainContext';
 import {Button, Text} from '@rneui/themed';
 
 export const Intro = ({navigation}) => {
-  const {promoStatus, setPromoStatus} = useMainContext();
+  const {setPromoStatus} = useMainContext();
   return (
     <View>
       <Text>I AM THE PROMO</Text>
@@ -20,8 +20,6 @@ export const Intro = ({navigation}) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({});
 
 Intro.propTypes = {
   navigation: PropTypes.object,
